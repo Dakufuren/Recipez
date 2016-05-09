e135c6b862e19c5ed0974e51c775a86baedd4feb
Axel Malmberg <axel_malmberg@hotmail.com>
2016-05-09 20:22:37 +0200

    minor changes

--- app/src/main/java/com/example/albin/recipez/LoginActivity.java
+++ app/src/main/java/com/example/albin/recipez/LoginActivity.java
@@ -2,12 +2,20 @@ package com.example.albin.recipez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
