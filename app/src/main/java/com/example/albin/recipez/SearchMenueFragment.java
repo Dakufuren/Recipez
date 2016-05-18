8776b997f8efdddc4427754b6d85ef8bbb60f374
Axel Malmberg <axel_malmberg@hotmail.com>
2016-05-18 16:03:45 +0200

    forrgot another file soz

---
+++ app/src/main/java/com/example/albin/recipez/SearchMenueFragment.java
@@ -0,0 +1,33 @@
+package com.example.albin.recipez;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchMenueFragment extends android.support.v4.app.Fragment {
+
+
+    public SearchMenueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search_menue_layout,container,false);
+        Bundle bundle = getArguments();
+
+
+        return view;
+    }
+
+}
