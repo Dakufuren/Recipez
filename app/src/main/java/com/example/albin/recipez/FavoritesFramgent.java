594c251e4f7cfd433af20a17b9db8e6cd5578771
Albin Åberg <albin.aberg@outlook.com>
2016-06-02 18:16:32 +0200

    ListView and the logic done for the future with favorites

--- app/src/main/java/com/example/albin/recipez/FavoritesFramgent.java
+++ app/src/main/java/com/example/albin/recipez/FavoritesFramgent.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +20,7 @@ import android.widget.Toast;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FavoritesFramgent extends android.support.v4.app.Fragment {
+public class FavoritesFramgent extends ListFragment {
 
     private ListView favList;
     private RecipeObject obj1 = new RecipeObject("Oumphs Special Blend", "vegan","vegetarian", "lorem dolar set amet", 25, "Oumph", "Onion", "Rice", "Avocado", "Beans");
@@ -37,35 +38,80 @@ public class FavoritesFramgent extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.fragment_favorites_framgent_layout, container,false);
         Bundle bundle = getArguments();
 
-        favList = (ListView) view.findViewById(R.id.FavoritesListView);
 
-        String[] strArr = new String[3];
+
+        return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        String[] strArr = new String[2];
 
         //ADD OBJECTS
 
         strArr[0] = obj1.getRecipeName();
         strArr[1] = obj2.getRecipeName();
+
         //ADD OBJECTS TITEL TO strArr
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.fragment_favorites_framgent_layout,
-                strArr);
-        favList.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, strArr);
+        setListAdapter(adapter);
 
-        favList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
                 int tempId = position;
+                String tempMsg = "";
+                String tempTitel = "";
+
+                if(position == 0){
+                    tempTitel = obj1.getRecipeName();
+                    tempMsg = "Description: \n" +
+                            obj1.getDescription() + "\n" +
+                            "\n" +
+                            "Ingredients: \n" +
+                            obj1.getIngredient1() + "\n" +
+                            obj1.getIngredient2() + "\n" +
+                            obj1.getIngredient3() + "\n" +
+                            obj1.getIngredient4() + "\n" +
+                            "\n" +
+                            "Categories: \n" +
+                            obj1.getRecipeCategory() + "\n" +
+                            obj1.getRecipeCategory2() + "\n" +
+                            "\n" +
+                            "Price: " +
+                            obj1.getPrice();
+                }
+
+                if(position == 1){
+                    tempTitel = obj2.getRecipeName();
+                    tempMsg = "Description: \n" +
+                            obj2.getDescription() + "\n" +
+                            "\n" +
+                            "Ingredients: \n" +
+                            obj2.getIngredient1() + "\n" +
+                            obj2.getIngredient2() + "\n" +
+                            obj2.getIngredient3() + "\n" +
+                            obj2.getIngredient4() + "\n" +
+                            "\n" +
+                            "Categories: \n" +
+                            obj2.getRecipeCategory() + "\n" +
+                            "\n" +
+                            "Price: " +
+                            obj2.getPrice();
+                }
+                System.out.println(position);
 
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
                         getActivity());
 
-                alertDialogBuilder.setTitle("Recipes");
+                alertDialogBuilder.setTitle(tempTitel);
 
                 // set dialog message
                 alertDialogBuilder
-                        .setMessage(obj1.getDescription())
+                        .setMessage(tempMsg)
                         .setCancelable(false)
                      /*   .setPositiveButton("Remove",new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,int id) {
@@ -93,9 +139,6 @@ public class FavoritesFramgent extends android.support.v4.app.Fragment {
             }
 
         });
-
-
-        return view;
     }
 
 }
